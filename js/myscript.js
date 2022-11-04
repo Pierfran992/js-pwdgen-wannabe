1c585c3cce5a00a6e6cdc83bacbb4285679192a2
Pierfran992 <santoro.pierfrancesco@hotmail.it>
2022-11-04 14:47:06 +0100

    inserite regole di debug nel file style.css e sviluppate  nel file myscript.js linee guida per lo sviluppo del progetto

---
+++ js/myscript.js
@@ -0,0 +1,16 @@
+// creare tramite l'inserimento del nome, del cognome e del colore preferito una password personalizzata per l'utente con un 21 finale fisso
+
+
+// chiedi all'utente il nome
+
+
+// chiedi all'utente il cognome
+
+
+// chiedi all'utente il suo colore preferito
+
+
+// genera la password avente questo formato generale - nomeCognomeColorepreferito21 -
+
+
+// output della password 
